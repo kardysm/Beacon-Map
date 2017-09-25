700e44d48744960518caf8d2b0a6ab9574415695
Michał Kardyś <draku29@gmail.com>
2017-09-25 20:24:05 +0200

    Add latlng bounds to beacons GET
    *Additionally, add beaconId to userdata retrieval by beacon route

--- resources/beacons/beforerequest.js
+++ resources/beacons/beforerequest.js
@@ -1,3 +1,37 @@
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+
+query.boundsError = false;
+if(typeof query.id === 'undefined' && ctx.req.method === 'GET'){
+    var areBoundsNumeric = isNumeric(query.minLat) 
+    && isNumeric(query.maxLat)
+    && isNumeric(query.minLng)
+    && isNumeric(query.maxLng);
+    if(areBoundsNumeric){
+        var isLatRangeValid = query.minLat < 90 
+        && query.minLat < query.maxLat
+        && query.maxLat > -90;
+        
+        query.minLng += 180;
+        query.minLng %= 360;
+        query.minLng -= 180;
+        
+        query.maxLng += 180;
+        query.maxLng %= 360;
+        query.maxLng -= 180;
+        
+        var isLngRangeValid = query.minLng < 180 
+        && query.minLng < query.maxLng
+        && query.maxLng > -180;
+    }
+    cancelUnless(areBoundsNumeric && isLatRangeValid && isLngRangeValid,'LatLng bounds contain invalid data',400)
+    
+    
+    query.latlng = {lat: {$gte: query.minLat, $lte: query.maxLat}, lng: {$gte: query.minLng, $lte: query.maxLng}};
+}
+
 if(!internal){
     dpd.users.get({key: query.key}, function(results,error){
         cancelUnless(results.length,"Key is not valid",401);
--- resources/beacons/get.js
+++ resources/beacons/get.js
@@ -1,7 +1,8 @@
-dpd.userdata.get({userId: query.userId},function(results,error){
+dpd.userdata.get({userId: query.userId, beaconId: this.id},function(results,error){
     var reduced = results.reduce(function(acc,current){
         acc.push(current.data);
         return acc;
     },[]);
     this.userData = reduced;
 });
+
